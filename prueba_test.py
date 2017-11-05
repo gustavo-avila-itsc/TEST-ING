bdd13e82ce169f1443d239640e5d86db0f6fc4ec
lucas <luk-s.17@hotmail.com.ar>
2017-11-05 20:14:32 -0300

    agrego test para sut

---
+++ prueba_test.py
@@ -0,0 +1,22 @@
+import unittest
+from sut import *
+
+class Test(unittest.TestCase):
+
+    def testarea(self):
+        self.assertTrue(area(2, 3) == 6)
+
+    def testsaludar(self):
+        self.assertTrue(saludar("gustavo") == "Hola gustavo")
+
+    def testsumar(self):
+        self.assertTrue(sumar(20, 15) == 35)
+	
+    def testvalorabsoluto(self):
+        self.assertTrue(valorabsoluto(-20) == 20)
+		
+    def testcostototal(self):
+        self.assertTrue(costototal(20, 15) == "El costo total es $35")
+
+if __name__ == '__main__':
+    unittest.main()
